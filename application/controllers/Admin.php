c4156a2ad496c10d3fd7e508eb457fdd6242f353
nusambatu <nusambatu@gmail.com>
2019-12-24 15:11:32 +0700

    ganti dasbord dengan halaman awal

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,7 +11,7 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        $data['title'] = 'Dashboard';
+        $data['title'] = 'halaman awal';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
         $this->load->view('templates/header', $data);
